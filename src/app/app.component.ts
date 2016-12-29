a46d3e582fb39f7eb0599bb9d4e6a45581e01ffd
Eddie Maas <eddie@edease.nl>
2016-12-29 09:08:03 +0100

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {
+  Component
+} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  selectedOption: string;
+
+
+  selects = [{
+    val : "jipjip"
+  }];
+
+  tiles = [{
+    text: 'One',
+    cols: 3,
+    rows: 1,
+    color: 'lightblue'
+  }, {
+    text: 'Two',
+    cols: 1,
+    rows: 2,
+    color: 'lightgreen'
+  }, {
+    text: 'Three',
+    cols: 1,
+    rows: 1,
+    color: 'lightpink'
+  }, {
+    text: 'Four',
+    cols: 2,
+    rows: 1,
+    color: '#DDBDF1'
+  }, ];
+
+  folders = [{
+    name: 'Photos',
+    updated: new Date('1/1/16'),
+  }, {
+    name: 'Recipes',
+    updated: new Date('1/17/16'),
+  }, {
+    name: 'Work',
+    updated: new Date('1/28/16'),
+  }];
+  notes = [{
+    name: 'Vacation Itinerary',
+    updated: new Date('2/20/16'),
+  }, {
+    name: 'Kitchen Remodel',
+    updated: new Date('1/18/16'),
+  }];
+
+  // function
+  openDialog() {
+    console.log("Tjakkaaaa");
+  }
 }
